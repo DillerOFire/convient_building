4fc62ca62f5649eb675565368604c870db0e26ad
VerpaPhullus <niktofe1@gmail.com>
2020-10-11 14:50:41 +1000

    Fixed

--- device_adapt.sh
+++ device_adapt.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+mv .git /tmp
+
 export oldstring=$1
 export newstring=$2
-grep -rl $oldstring . | xargs sed -i "s/$oldstring/$newstring/g" &&
-mv $oldstring_lavender.mk $newstring_lavender.mk &&
+grep -rl $oldstring . | xargs sed -i "s/$oldstring/$newstring/g"
+mv $oldstring'_lavender.mk' $newstring'_lavender.mk'
 mv $oldstring.dependencies $newstring.dependencies
+
+mv /tmp/.git .
+git add .
